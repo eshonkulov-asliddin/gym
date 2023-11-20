289a983d9f2fe2b14ef365067cbca62cc44ac7d1
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-20 21:26:01 +0500

    Add Reader and ReaderTest

---
+++ src/main/java/dev/gym/repository/datasource/reader/impl/Utils.java
@@ -0,0 +1,8 @@
+package dev.gym.repository.datasource.reader.impl;
+
+public class Utils {
+    static final String READ_DATA_FROM_FILE = "Read %s from file: %s";
+    static final String ADD_DATA_TO_LIST = "Add %s to list: %s";
+    static final String ERROR_READING_FILE = "Error reading file: %s";
+    static final String PARSE_FROM_FILE = "Parse %s from file: %s";
+}
