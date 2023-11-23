8e59ea13c82e096be98195c248c93622abab84e7
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-23 15:01:39 +0500

    Update isEmpty() to size()

--- src/test/java/dev/gym/repository/datasource/InMemoryDatabaseConfigTest.java
+++ src/test/java/dev/gym/repository/datasource/InMemoryDatabaseConfigTest.java
@@ -8,25 +8,27 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringJUnitConfig(classes = RepositoryConfig.class)
 class InMemoryDatabaseConfigTest {
 
-    @Autowired private ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
     @Test
-    void testInMemoryDatabase(){
+    void givenInMemoryStores_whenStoresIsNotEmpty_thenOk(){
 
         var trainer = context.getBean("trainerStore", Map.class);
         var trainee = context.getBean("traineeStore", Map.class);
         var training = context.getBean("trainingStore", Map.class);
         var username = context.getBean("usernameStore", Map.class);
 
-            assertFalse(trainer.isEmpty());
-            assertFalse(trainee.isEmpty());
-            assertFalse(training.isEmpty());
-            assertFalse(username.isEmpty());
+        int expectedSize = 0;
+        assertTrue(trainer.size() > expectedSize);
+        assertTrue(trainee.size() > expectedSize);
+        assertTrue(training.size() > expectedSize);
+        assertTrue(username.size() > expectedSize);
     }
 
 }
