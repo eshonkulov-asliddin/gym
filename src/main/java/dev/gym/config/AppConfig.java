e39e4dc4c8068438a501f369ebe0cef0cfb92cbe
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-23 14:58:00 +0500

    Update AppConfig

---
+++ src/main/java/dev/gym/config/AppConfig.java
@@ -0,0 +1,9 @@
+package dev.gym.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "dev.gym")
+public class AppConfig {
+}
