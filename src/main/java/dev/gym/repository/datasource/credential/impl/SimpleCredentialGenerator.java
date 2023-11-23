17953ce54451affa674c833d5ba3818706d862fb
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-23 14:45:24 +0500

    Updat generatePassword() with RandomStringUtils

--- src/main/java/dev/gym/repository/datasource/credential/impl/SimpleCredentialGenerator.java
+++ src/main/java/dev/gym/repository/datasource/credential/impl/SimpleCredentialGenerator.java
@@ -1,6 +1,7 @@
 package dev.gym.repository.datasource.credential.impl;
 
 import dev.gym.repository.datasource.credential.CredentialGenerator;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import java.util.Map;
 
 @Component
 public class SimpleCredentialGenerator implements CredentialGenerator {
+    public static final String DELIMITER = ".";
     private final Map<String, Integer> usernameStore;
 
     @Autowired
@@ -17,7 +19,7 @@ public class SimpleCredentialGenerator implements CredentialGenerator {
 
     @Override
     public String generateUsername(String firstName, String lastName) {
-        String baseUsername = firstName + "." + lastName;
+        String baseUsername = firstName + DELIMITER + lastName;
         int count = usernameStore.getOrDefault(baseUsername, 0);
 
         String generatedUsername = (count == 0) ? baseUsername : baseUsername + count;
@@ -33,12 +35,6 @@ public class SimpleCredentialGenerator implements CredentialGenerator {
 
     @Override
     public String generatePassword() {
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder passwordBuilder = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            int index = (int) (Math.random() * chars.length());
-            passwordBuilder.append(chars.charAt(index));
-        }
-        return passwordBuilder.toString();
+        return RandomStringUtils.randomAlphabetic (10);
     }
 }
