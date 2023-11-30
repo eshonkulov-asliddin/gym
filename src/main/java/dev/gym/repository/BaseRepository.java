6713fe2a1bece73b85961d2c916353c838aa12e3
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-30 18:48:31 +0500

    Adjust repository for Hibernate

--- src/main/java/dev/gym/repository/BaseRepository.java
+++ src/main/java/dev/gym/repository/BaseRepository.java
@@ -1,11 +1,12 @@
 package dev.gym.repository;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
 public interface BaseRepository<T, K> {
-    Map<K, T> findAll();
+    List<T> findAll();
     Optional<T> findById(K id);
     T save(T entity);
     boolean deleteById(K id);
+    Optional<T> findProxyById(K id);
 }
--- src/main/java/dev/gym/repository/impl/TrainerRepository.java
+++ src/main/java/dev/gym/repository/impl/TrainerRepository.java
@@ -1,34 +1,8 @@
 package dev.gym.repository.impl;
 
 import dev.gym.model.Trainer;
-import dev.gym.repository.datasource.reader.FileReader;
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.Map;
 
 @Repository
 public class TrainerRepository extends AbstractRepository<Trainer, Long> {
-    private static final Logger logger = LoggerFactory.getLogger(TrainerRepository.class);
-    @Getter
-    private final Map<Long, Trainer> data;
-    private final FileReader<Trainer> fileReader;
-
-    @Autowired
-    TrainerRepository(Map<Long, Trainer> data,
-                      FileReader<Trainer> fileReader) {
-        super(logger, data);
-        this.data = data;
-        this.fileReader = fileReader;
-    }
-    @PostConstruct
-    private void loadTrainers(){
-       fileReader.read().forEach(trainer -> {
-           data.put(trainer.getId(), trainer);
-       });
-    }
 }
