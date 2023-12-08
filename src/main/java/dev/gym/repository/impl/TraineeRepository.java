ed7cf66babf8c3c1f66e73cc90e4b8649465bd1f
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:41:45 +0500

    Update repository implementations

--- src/main/java/dev/gym/repository/impl/TraineeRepository.java
+++ src/main/java/dev/gym/repository/impl/TraineeRepository.java
@@ -1,9 +1,15 @@
 package dev.gym.repository.impl;
 
 import dev.gym.model.Trainee;
+import dev.gym.model.Training;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TraineeRepository extends AbstractRepository<Trainee, Long> {
+public class TraineeRepository extends AbstractUserRepository<Trainee, Training, Long> {
+
+    TraineeRepository(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
 
 }
--- src/main/java/dev/gym/repository/impl/TrainerRepository.java
+++ src/main/java/dev/gym/repository/impl/TrainerRepository.java
@@ -1,8 +1,15 @@
 package dev.gym.repository.impl;
 
 import dev.gym.model.Trainer;
+import dev.gym.model.Training;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TrainerRepository extends AbstractRepository<Trainer, Long> {
+public class TrainerRepository extends AbstractUserRepository<Trainer, Training, Long> {
+
+    TrainerRepository(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
+
 }
