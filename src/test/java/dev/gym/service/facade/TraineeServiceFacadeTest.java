e6f9a57a54365ee56126aef7fa12027688036e0b
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 11:04:30 +0500

    Add facade tests

---
+++ src/test/java/dev/gym/service/facade/TraineeServiceFacadeTest.java
@@ -0,0 +1,30 @@
+package dev.gym.service.facade;
+
+import dev.gym.config.AppConfig;
+import dev.gym.model.Trainee;
+import dev.gym.model.Training;
+import dev.gym.service.dto.TraineeDtoRequest;
+import dev.gym.service.dto.TraineeDtoResponse;
+import dev.gym.service.exception.InvalidUsernameOrPasswordException;
+import dev.gym.service.facade.impl.AbstractUserFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringJUnitConfig(classes = {AppConfig.class})
+class TraineeServiceFacadeTest {
+
+    @Autowired
+    protected AbstractUserFacade<TraineeDtoRequest, TraineeDtoResponse, Long, Trainee, Training> traineeServiceFacade;
+
+    @Test
+    void givenWrongUsernameAndPassword_whenGetAll_thenThrowInvalidUsernameOrPasswordException() {
+        String USERNAME = "wrongUsername";
+        String PASSWORD = "wrongPassword";
+
+        assertThrows(InvalidUsernameOrPasswordException.class, () -> traineeServiceFacade.getAll(USERNAME, PASSWORD));
+    }
+
+}
---
+++ src/test/java/dev/gym/service/facade/TrainerServiceFacadeTest.java
@@ -0,0 +1,30 @@
+package dev.gym.service.facade;
+
+import dev.gym.config.AppConfig;
+import dev.gym.model.Trainer;
+import dev.gym.model.Training;
+import dev.gym.service.dto.TrainerDtoRequest;
+import dev.gym.service.dto.TrainerDtoResponse;
+import dev.gym.service.exception.InvalidUsernameOrPasswordException;
+import dev.gym.service.facade.impl.AbstractUserFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringJUnitConfig(classes = {AppConfig.class})
+class TrainerServiceFacadeTest {
+
+    @Autowired
+    protected AbstractUserFacade<TrainerDtoRequest, TrainerDtoResponse, Long, Trainer, Training> trainerServiceFacade;
+
+    @Test
+    void givenWrongUsernameAndPassword_whenDelete_thenThrowInvalidUsernameOrPasswordException() {
+        String USERNAME = "wrongUsername";
+        String PASSWORD = "wrongPassword";
+
+        assertThrows(InvalidUsernameOrPasswordException.class, () -> trainerServiceFacade.deleteByUsername(USERNAME, PASSWORD));
+    }
+
+}
