a08acb2992059160af1de1b9468e676994630e8e
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:31:59 +0500

    Update RepositoryConfig

--- src/main/java/dev/gym/repository/config/RepositoryConfig.java
+++ src/main/java/dev/gym/repository/config/RepositoryConfig.java
@@ -2,11 +2,8 @@ package dev.gym.repository.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "dev.gym.repository")
-@PropertySource("classpath:application.properties")
 public class RepositoryConfig {
-
 }
