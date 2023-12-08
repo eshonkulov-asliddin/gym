f27a4801a21c97f5bf7c7ce3cc0f739b4a222278
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:32:27 +0500

    Removed in-memory db

--- src/main/java/dev/gym/repository/datasource/DatabaseConfig.java
+++ src/main/java/dev/gym/repository/datasource/DatabaseConfig.java
@@ -5,18 +5,10 @@ import jakarta.persistence.Persistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
-public class InMemoryDatabaseConfig {
+public class DatabaseConfig {
     @Bean
-    public Map<String, Integer> usernameStore(){
-        return new HashMap<>();
-    }
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory(){
+    public EntityManagerFactory entityManagerFactory() {
         return Persistence.createEntityManagerFactory("GYM");
     }
 }
