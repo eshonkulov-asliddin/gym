30f22a606adb33ba2d07fcfd9c0f76bc8b600cdd
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:36:24 +0500

    Renamed repository name

--- src/main/java/dev/gym/repository/CrudRepository.java
+++ src/main/java/dev/gym/repository/CrudRepository.java
@@ -3,10 +3,16 @@ package dev.gym.repository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BaseRepository<T, K> {
+public interface CrudRepository<T, K> {
+
     List<T> findAll();
+
     Optional<T> findById(K id);
+
     T save(T entity);
-    boolean deleteById(K id);
-    Optional<T> findProxyById(K id);
+
+    boolean delete(K id);
+
+    Optional<T> findReferenceById(K id);
+
 }
