bacd1842af8bcdd3305913ec24fb03cab387213f
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:33:59 +0500

    Update credential-generator implementation

--- src/main/java/dev/gym/repository/datasource/credential/impl/SimpleCredentialGenerator.java
+++ src/main/java/dev/gym/repository/datasource/credential/impl/SimpleCredentialGenerator.java
@@ -1,40 +1,49 @@
 package dev.gym.repository.datasource.credential.impl;
 
 import dev.gym.repository.datasource.credential.CredentialGenerator;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.HashSet;
 
 @Component
 public class SimpleCredentialGenerator implements CredentialGenerator {
     public static final String DELIMITER = ".";
-    private final Map<String, Integer> usernameStore;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Autowired
-    public SimpleCredentialGenerator(Map<String, Integer> usernameStore) {
-        this.usernameStore = usernameStore;
+    public SimpleCredentialGenerator(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Override
     public String generateUsername(String firstName, String lastName) {
         String baseUsername = firstName + DELIMITER + lastName;
-        int count = usernameStore.getOrDefault(baseUsername, 0);
-
-        String generatedUsername = (count == 0) ? baseUsername : baseUsername + count;
-
-        while (usernameStore.containsKey(generatedUsername)){
+        HashSet<String> usernameSet = new HashSet<>();
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            String query = "SELECT u.username FROM User u WHERE u.username LIKE :username";
+            usernameSet = new HashSet<>(entityManager.createQuery(query, String.class)
+                    .setParameter("username", baseUsername + "%")
+                    .getResultList());
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+        }
+        int count = 0;
+        String generatedUsername = baseUsername;
+        while (usernameSet.contains(generatedUsername)) {
             count++;
             generatedUsername = baseUsername + count;
         }
 
-        usernameStore.put(generatedUsername, 1);
         return generatedUsername;
     }
 
     @Override
     public String generatePassword() {
-        return RandomStringUtils.randomAlphabetic (10);
+        return RandomStringUtils.randomAlphabetic(10);
     }
 }
