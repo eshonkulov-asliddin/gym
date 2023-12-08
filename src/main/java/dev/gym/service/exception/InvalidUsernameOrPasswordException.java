ea4757f009d56ad6b12272438e62b59095be6bd1
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:49:23 +0500

    Update exceptions

---
+++ src/main/java/dev/gym/service/exception/InvalidUsernameOrPasswordException.java
@@ -0,0 +1,18 @@
+package dev.gym.service.exception;
+
+
+public class InvalidUsernameOrPasswordException extends RuntimeException {
+
+    public InvalidUsernameOrPasswordException(String message) {
+        super(message);
+    }
+
+    public InvalidUsernameOrPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidUsernameOrPasswordException(Throwable cause) {
+        super(cause);
+    }
+
+}
