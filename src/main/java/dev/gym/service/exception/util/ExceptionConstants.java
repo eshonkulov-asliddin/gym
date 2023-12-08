f69d20670d903ebc67de697be506a49af51f536d
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-08 10:48:22 +0500

    Add ExceptionConstants

---
+++ src/main/java/dev/gym/service/exception/util/ExceptionConstants.java
@@ -0,0 +1,13 @@
+package dev.gym.service.exception.util;
+
+public class ExceptionConstants {
+
+    public static String NOT_FOUND_MESSAGE = "not found %s with id %s";
+
+    public static String ILLIGAL_ARGUMENT_MESSAGE = "% is not valid";
+
+    public static String SAVE_FAILED_MESSAGE = "couldn't save %s with id %s";
+
+    public static String INVALID_USERNAME_OR_PASSWORD_MESSAGE = "invalid username or password";
+
+}
