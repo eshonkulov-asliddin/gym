e4ad25ff8fe3ff30fa9903aed0356cfe88095699
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-13 19:24:24 +0500

    Add unit-test

--- src/test/java/dev/gym/repository/TraineeRepositoryTest.java
+++ src/test/java/dev/gym/repository/TraineeRepositoryTest.java
@@ -1,143 +1,73 @@
 package dev.gym.repository;
 
-import dev.gym.config.AppConfig;
 import dev.gym.model.Trainee;
-import dev.gym.model.Trainer;
-import dev.gym.model.Training;
-import dev.gym.model.TrainingType;
-import dev.gym.model.enums.TrainingTypeEnum;
-import dev.gym.repository.datasource.credential.CredentialGenerator;
 import dev.gym.repository.impl.AbstractCrudRepository;
-import dev.gym.repository.impl.AbstractUserRepository;
+import dev.gym.repository.impl.TraineeRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import java.time.LocalDate;
-import java.util.Optional;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@SpringJUnitConfig(classes = AppConfig.class)
 class TraineeRepositoryTest {
 
-    @Autowired
-    protected AbstractUserRepository<Trainee, Training, Long> traineeRepository;
-
-    @Autowired
-    protected AbstractCrudRepository<Training, Long> trainingRepository;
-
-    @Autowired
-    protected CredentialGenerator credentialGenerator;
-
-    private Trainee savedTrainee;
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+    private EntityTransaction entityTransaction;
+    private AbstractCrudRepository<Trainee, Long> traineeRepository;
+    private TypedQuery<Object> typedQuery;
 
     @BeforeEach
     void setUp() {
-        String FIRSTNAME = "John";
-        String LASTNAME = "Doe";
-        LocalDate DATE_OF_BIRTH = LocalDate.of(1990, 1, 1);
+        entityManagerFactory = mock(EntityManagerFactory.class);
+        entityManager = mock(EntityManager.class);
+        entityTransaction = mock(EntityTransaction.class);
+        typedQuery = mock(TypedQuery.class);
 
+        when(entityManagerFactory.createEntityManager()).thenReturn(entityManager);
+        when(entityManager.getTransaction()).thenReturn(entityTransaction);
+
+        traineeRepository = new TraineeRepository(entityManagerFactory);
+    }
+
+    @Test
+    void testFindAll() {
+        when(entityManager.createQuery(any(), any())).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(List.of(new Trainee()));
+
+        assertEquals(traineeRepository.findAll().size(), 1);
+    }
+
+    @Test
+    void testSave() {
         Trainee trainee = new Trainee();
-        trainee.setFirstName(FIRSTNAME);
-        trainee.setLastName(LASTNAME);
-        trainee.setUsername(credentialGenerator.generateUsername(FIRSTNAME, LASTNAME));
-        trainee.setPassword(credentialGenerator.generatePassword());
-        trainee.setDateOfBirth(DATE_OF_BIRTH);
+        trainee.setFirstName("John");
+        trainee.setLastName("Doe");
 
-        savedTrainee = traineeRepository.save(trainee);
+        doNothing().when(entityManager).persist(trainee);
+
+        Trainee save = traineeRepository.save(trainee);
+
+        assertEquals(save.getFirstName(), trainee.getFirstName());
     }
 
     @Test
-    void givenValidEntity_whenSave_thenSuccess() {
-        assertNotNull(savedTrainee);
+    void testDelete() {
+        Trainee trainee = new Trainee();
+
+        when(entityManager.getReference(any(), any())).thenReturn(trainee);
+        doNothing().when(entityManager).remove(trainee);
+
+        assertTrue(traineeRepository.delete(1L));
     }
-
-    @Test
-    void givenValidId_whenFindById_thenSuccess() {
-        Optional<Trainee> byId = traineeRepository.findById(savedTrainee.getId());
-        assertTrue(byId.isPresent());
-    }
-
-    @Test
-    void givenValidUsername_whenFindByUsername_thenSuccess() {
-        Optional<Trainee> byUsername = traineeRepository.findByUsername(savedTrainee.getUsername());
-        assertTrue(byUsername.isPresent());
-    }
-
-    @Test
-    void givenInValidUsername_whenFindByUsername_thenNothingReturned() {
-        Optional<Trainee> byUsername = traineeRepository.findByUsername("invalid");
-        assertTrue(byUsername.isEmpty());
-    }
-
-    @Test
-    void givenValidUsername_whenDeleteByUsername_thenDelete() {
-        traineeRepository.deleteByUsername(savedTrainee.getUsername());
-        Optional<Trainee> byUsername = traineeRepository.findByUsername(savedTrainee.getUsername());
-        assertTrue(byUsername.isEmpty());
-    }
-
-    @Test
-    void givenValidId_whenUpdatePassword_thenUpdate() {
-        String newPassword = credentialGenerator.generatePassword();
-        traineeRepository.updatePassword(savedTrainee.getId(), newPassword);
-        Optional<Trainee> byId = traineeRepository.findById(savedTrainee.getId());
-        assertTrue(byId.isPresent());
-        assertEquals(byId.get().getPassword(), newPassword);
-    }
-
-    @Test
-    void givenInvalidId_whenUpdatePassword_thenPasswordNotChanged() {
-        String newPassword = credentialGenerator.generatePassword();
-        traineeRepository.updatePassword(-1L, newPassword);
-        Optional<Trainee> byId = traineeRepository.findById(savedTrainee.getId());
-        assertTrue(byId.isPresent());
-        assertEquals(byId.get().getPassword(), savedTrainee.getPassword());
-    }
-
-    @Test
-    void whenSetActiveStatus_thenStatusChanged() {
-        traineeRepository.setActiveStatus(savedTrainee.getId(), false);
-        Optional<Trainee> byId = traineeRepository.findById(savedTrainee.getId());
-        assertTrue(byId.isPresent());
-        assertFalse(byId.get().isActive());
-    }
-
-    @Test
-    void givenValidUsername_whenFindAllTrainingsByUsername_thenSuccess() {
-        Training training = createTraining();
-        assertFalse(traineeRepository.findAllTrainingsByUsername(savedTrainee.getUsername()).isEmpty());
-    }
-
-    private Training createTraining() {
-        // Fields for Trainer
-        String TRAINER_FIRSTNAME = "Tom";
-        String TRAINER_LASTNAME = "Jerry";
-
-        Trainer trainer = new Trainer();
-        trainer.setFirstName(TRAINER_FIRSTNAME);
-        trainer.setLastName(TRAINER_LASTNAME);
-        trainer.setUsername(credentialGenerator.generateUsername(TRAINER_FIRSTNAME, TRAINER_LASTNAME));
-        trainer.setPassword(credentialGenerator.generatePassword());
-
-        // Fields for TrainingType
-        TrainingType trainingType = new TrainingType();
-        trainingType.setTrainingTypeName(TrainingTypeEnum.STRENGTH);
-
-
-        Training training = new Training();
-        training.setTrainee(savedTrainee);
-        training.setTrainer(trainer);
-        training.setTrainingName("Test Training");
-        training.setTrainingType(trainingType);
-        training.setTrainingDate(LocalDate.of(2024, 1, 1));
-        training.setTrainingDuration(60);
-        return trainingRepository.save(training);
-    }
-
 }
