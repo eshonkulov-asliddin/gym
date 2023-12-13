02cd1a25f5b80ed5b2571d1eff16a696ce766c37
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-13 19:19:04 +0500

    Update dtos

--- src/main/java/dev/gym/service/dto/TrainerDtoRequest.java
+++ src/main/java/dev/gym/service/dto/TrainerDtoRequest.java
@@ -1,5 +1,5 @@
 package dev.gym.service.dto;
 
-public record TrainerDtoRequest(String firstName, String lastName, SpecializationDto specialization) {
+public record TrainerDtoRequest(String firstName, String lastName, TrainingTypeDto specialization) {
 
 }
--- src/main/java/dev/gym/service/dto/TrainerDtoResponse.java
+++ src/main/java/dev/gym/service/dto/TrainerDtoResponse.java
@@ -1,5 +1,5 @@
 package dev.gym.service.dto;
 
-public record TrainerDtoResponse(String firstName, String lastName, String username, SpecializationDto specialization) {
+public record TrainerDtoResponse(String firstName, String lastName, String username, TrainingTypeDto specialization) {
 
 }
