1860a0fc53bb4fa8897ea45b03b17d82987c785a
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-27 19:22:40 +0500

    Renamed repository

--- src/main/java/dev/gym/repository/impl/UserRepositoryImpl.java
+++ src/main/java/dev/gym/repository/impl/UserRepositoryImpl.java
@@ -1,16 +1,16 @@
 package dev.gym.repository.impl;
 
-import dev.gym.model.Training;
+import dev.gym.repository.UserRepository;
+import dev.gym.repository.model.User;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TrainingRepository extends AbstractCrudRepository<Training, Long> {
+public class UserRepositoryImpl extends AbstractUserRepository<User, Long> implements UserRepository<User, Long> {
 
     @Autowired
-    public TrainingRepository(EntityManagerFactory entityManagerFactory) {
+    public UserRepositoryImpl(EntityManagerFactory entityManagerFactory) {
         super(entityManagerFactory);
     }
-
 }
