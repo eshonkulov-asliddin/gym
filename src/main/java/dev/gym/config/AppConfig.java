0b19723af8160a3292fadd32bf5b7b43853321fe
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-12-27 19:08:13 +0500

    Enable Web Mvc

--- src/main/java/dev/gym/config/AppConfig.java
+++ src/main/java/dev/gym/config/AppConfig.java
@@ -2,8 +2,10 @@ package dev.gym.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "dev.gym")
 public class AppConfig {
 }
