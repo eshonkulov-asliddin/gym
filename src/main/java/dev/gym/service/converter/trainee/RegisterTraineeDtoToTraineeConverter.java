ad2d8dda9966f10b03a2b54c787d63755d71f066
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 11:12:53 +0500

    Add lombok RequiredArgsConstructor

--- src/main/java/dev/gym/service/converter/trainee/RegisterTraineeDtoToTraineeConverter.java
+++ src/main/java/dev/gym/service/converter/trainee/RegisterTraineeDtoToTraineeConverter.java
@@ -3,17 +3,15 @@ package dev.gym.service.converter.trainee;
 import dev.gym.repository.datasource.credential.CredentialGenerator;
 import dev.gym.repository.model.Trainee;
 import dev.gym.service.dto.RegisterTraineeDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class RegisterTraineeDtoToTraineeConverter implements Converter<RegisterTraineeDto, Trainee> {
 
     private final CredentialGenerator credentialGenerator;
-
-    public RegisterTraineeDtoToTraineeConverter(CredentialGenerator credentialGenerator) {
-        this.credentialGenerator = credentialGenerator;
-    }
 
     @Override
     public Trainee convert(RegisterTraineeDto source) {
