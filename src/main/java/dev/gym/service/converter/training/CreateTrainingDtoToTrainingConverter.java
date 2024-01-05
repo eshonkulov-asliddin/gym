5652086db53198441df4b5fc94dd9f6014d5ed1d
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 11:14:55 +0500

    Update CreateTrainingDtoToTrainingConverter

--- src/main/java/dev/gym/service/converter/training/CreateTrainingDtoToTrainingConverter.java
+++ src/main/java/dev/gym/service/converter/training/CreateTrainingDtoToTrainingConverter.java
@@ -1,17 +1,17 @@
 package dev.gym.service.converter.training;
 
 import dev.gym.repository.model.Training;
-import dev.gym.service.dto.AddTrainingDto;
+import dev.gym.service.dto.CreateTrainingDto;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CreateTrainingDtoToTrainingConverter implements Converter<AddTrainingDto, Training> {
+public class CreateTrainingDtoToTrainingConverter implements Converter<CreateTrainingDto, Training> {
+
     @Override
-    public Training convert(AddTrainingDto source) {
+    public Training convert(CreateTrainingDto source) {
         Training training = new Training();
         training.setTrainingName(source.trainingName());
-//        training.setTrainingType(TrainingTypeEnum.STRENGTH); // ask from mentor
         training.setTrainingDate(source.trainingDate());
         training.setTrainingDuration(source.trainingDuration());
         return training;
