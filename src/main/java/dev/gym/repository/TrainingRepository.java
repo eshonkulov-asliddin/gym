c03ee5429e665d266c22350b336152b1dfff3108
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 10:00:57 +0500

    Add findFor method

--- src/main/java/dev/gym/repository/TrainingRepository.java
+++ src/main/java/dev/gym/repository/TrainingRepository.java
@@ -2,6 +2,10 @@ package dev.gym.repository;
 
 import dev.gym.repository.model.Training;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TrainingRepository extends CrudRepository<Training, Long> {
 
+    List<Training> findFor(String traineeUsername, String trainerUsername, LocalDate from, LocalDate to);
 }
--- src/main/java/dev/gym/repository/impl/TrainingRepositoryImpl.java
+++ src/main/java/dev/gym/repository/impl/TrainingRepositoryImpl.java
@@ -6,11 +6,36 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static dev.gym.repository.impl.util.TransactionUtil.executeInTransaction;
+
 @Repository
 public class TrainingRepositoryImpl extends AbstractCrudRepository<Training, Long> implements TrainingRepository {
 
     @Autowired
     public TrainingRepositoryImpl(EntityManagerFactory entityManagerFactory) {
         super(entityManagerFactory);
+    }
+
+    @Override
+    public List<Training> findFor(String traineeUsername, String trainerUsername, LocalDate from, LocalDate to) {
+        return executeInTransaction(entityManagerFactory, entityManager -> {
+            String query = "SELECT t FROM Training t " +
+                    "JOIN FETCH t.trainee " +
+                    "JOIN FETCH t.trainer " +
+                    "WHERE (:traineeUsername IS NULL OR t.trainee.username = :traineeUsername) " +
+                    "AND (:trainerUsername IS NULL OR t.trainer.username = :trainerUsername) " +
+                    "AND (:from IS NULL OR t.trainingDate >= :from) " +
+                    "AND (:to IS NULL OR t.trainingDate <= :to) ";
+
+            return entityManager.createQuery(query, Training.class)
+                    .setParameter("traineeUsername", traineeUsername)
+                    .setParameter("trainerUsername", trainerUsername)
+                    .setParameter("from", from)
+                    .setParameter("to", to)
+                    .getResultList();
+        });
     }
 }
