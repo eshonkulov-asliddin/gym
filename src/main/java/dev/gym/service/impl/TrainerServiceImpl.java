d03e8e3761fab392dd66cb4e93ae14028714e482
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 19:24:12 +0500

    Add credential generator and trainingType selection logic

--- src/main/java/dev/gym/service/impl/TrainerServiceImpl.java
+++ src/main/java/dev/gym/service/impl/TrainerServiceImpl.java
@@ -2,8 +2,11 @@ package dev.gym.service.impl;
 
 import dev.gym.repository.TrainerRepository;
 import dev.gym.repository.TrainingRepository;
+import dev.gym.repository.TrainingTypeRepository;
+import dev.gym.repository.datasource.credential.CredentialGenerator;
 import dev.gym.repository.model.Trainer;
 import dev.gym.repository.model.Training;
+import dev.gym.repository.model.enums.TrainingTypeEnum;
 import dev.gym.service.TrainerService;
 import dev.gym.service.dto.RegisterTrainerDto;
 import dev.gym.service.dto.TrainerDto;
@@ -24,25 +27,42 @@ public class TrainerServiceImpl extends AbstractUserService<TrainerDto, Long, Tr
 
     private final TrainerRepository trainerRepository;
     private final TrainingRepository trainingRepository;
+    private final TrainingTypeRepository trainingTypeRepository;
+    private final CredentialGenerator credentialGenerator;
     private final ConversionService conversionService;
 
     @Autowired
     protected TrainerServiceImpl(TrainerRepository trainerRepository,
                                  TrainingRepository trainingRepository,
+                                 TrainingTypeRepository trainingTypeRepository,
+                                 CredentialGenerator credentialGenerator,
                                  ConversionService conversionService) {
         super(trainerRepository, conversionService);
         this.trainerRepository = trainerRepository;
         this.trainingRepository = trainingRepository;
+        this.trainingTypeRepository = trainingTypeRepository;
+        this.credentialGenerator = credentialGenerator;
         this.conversionService = conversionService;
     }
 
     public UserDto register(RegisterTrainerDto request) {
+        // Convert the specialization string to the corresponding enum
+        String trainerSpecializationString = request.specialization();
+        TrainingTypeEnum trainingTypeEnum = TrainingTypeEnum.valueOf(trainerSpecializationString);
+
         Trainer trainer = conversionService.convert(request, Trainer.class);
+        trainer.setUsername(credentialGenerator.generateUsername(trainer.getFirstName(), trainer.getLastName()));
+        trainer.setPassword(credentialGenerator.generatePassword());
+        trainer.setSpecialization(trainingTypeRepository.findByType(trainingTypeEnum));
         save(trainer);
         return conversionService.convert(trainer, UserDto.class);
     }
 
     public TrainerDto update(String username, UpdateTrainerDto request) {
+        // Convert the specialization string to the corresponding enum
+        String trainerSpecializationString = request.specialization();
+        TrainingTypeEnum trainerSpecializationEnum = TrainingTypeEnum.valueOf(trainerSpecializationString);
+
         Trainer oldTrainer = userRepository.findByUsername(username)
                 .orElseThrow(
                         () -> new NotFoundException(
@@ -50,6 +70,7 @@ public class TrainerServiceImpl extends AbstractUserService<TrainerDto, Long, Tr
                         )
                 );
         Trainer newTrainer = conversionService.convert(request, Trainer.class);
+        newTrainer.setSpecialization(trainingTypeRepository.findByType(trainerSpecializationEnum));
         newTrainer.setUsername(username);
         oldTrainer.update(newTrainer);
         save(oldTrainer);
