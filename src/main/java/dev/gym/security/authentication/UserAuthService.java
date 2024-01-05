84c9e293993392c6cd8a23ac918f9b6f4799b9f7
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 10:02:06 +0500

    Add lombok RequiredArgsConstructor

--- src/main/java/dev/gym/security/authentication/UserAuthService.java
+++ src/main/java/dev/gym/security/authentication/UserAuthService.java
@@ -5,18 +5,14 @@ import dev.gym.repository.model.User;
 import dev.gym.service.exception.InvalidUsernameOrPasswordException;
 import dev.gym.service.exception.util.ExceptionConstants;
 import jakarta.persistence.NoResultException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserAuthService implements AuthService {
 
     private final UserRepository<User, Long> userRepository;
-
-   @Autowired
-    public UserAuthService(UserRepository<User, Long> userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public void authenticate(String username, String password) throws InvalidUsernameOrPasswordException {
