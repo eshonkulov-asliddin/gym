6b8d7f24911af588246abe0566b51d79cadd4706
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 19:22:00 +0500

    Add lombok RequiredArgsConstructor

--- src/main/java/dev/gym/service/converter/trainer/UpdateTrainerDtoToTrainerConverter.java
+++ src/main/java/dev/gym/service/converter/trainer/UpdateTrainerDtoToTrainerConverter.java
@@ -1,6 +1,5 @@
 package dev.gym.service.converter.trainer;
 
-import dev.gym.repository.TrainingTypeRepository;
 import dev.gym.repository.model.Trainer;
 import dev.gym.service.dto.UpdateTrainerDto;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +8,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UpdateTrainerDtoRequestToTrainerConverter implements Converter<UpdateTrainerDto, Trainer> {
-
-    private TrainingTypeRepository trainingTypeRepository;
+public class UpdateTrainerDtoToTrainerConverter implements Converter<UpdateTrainerDto, Trainer> {
 
     @Override
     public Trainer convert(UpdateTrainerDto source) {
         Trainer trainer = new Trainer();
         trainer.setFirstName(source.firstName());
         trainer.setLastName(source.lastName());
-        trainer.setSpecialization(trainingTypeRepository.findByName(source.specialization()));
         trainer.setActive(source.isActive());
         return trainer;
     }
