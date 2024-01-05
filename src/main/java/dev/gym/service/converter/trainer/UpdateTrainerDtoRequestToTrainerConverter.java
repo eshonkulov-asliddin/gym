285f50185d9121756560937e27d562f741d37b13
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 11:13:57 +0500

    Add TrainingRepository

--- src/main/java/dev/gym/service/converter/trainer/UpdateTrainerDtoRequestToTrainerConverter.java
+++ src/main/java/dev/gym/service/converter/trainer/UpdateTrainerDtoRequestToTrainerConverter.java
@@ -1,19 +1,24 @@
 package dev.gym.service.converter.trainer;
 
+import dev.gym.repository.TrainingTypeRepository;
 import dev.gym.repository.model.Trainer;
 import dev.gym.service.dto.UpdateTrainerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UpdateTrainerDtoRequestToTrainerConverter implements Converter<UpdateTrainerDto, Trainer> {
+
+    private TrainingTypeRepository trainingTypeRepository;
 
     @Override
     public Trainer convert(UpdateTrainerDto source) {
         Trainer trainer = new Trainer();
         trainer.setFirstName(source.firstName());
         trainer.setLastName(source.lastName());
-        trainer.setSpecialization(source.specialization());
+        trainer.setSpecialization(trainingTypeRepository.findByName(source.specialization()));
         trainer.setActive(source.isActive());
         return trainer;
     }
