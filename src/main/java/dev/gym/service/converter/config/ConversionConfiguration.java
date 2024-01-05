ccaa2ac3e7257c9371bf6ce031ba2c55856048a6
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-05 19:19:22 +0500

    Update ConversionConfiguration

--- src/main/java/dev/gym/service/converter/config/ConversionConfiguration.java
+++ src/main/java/dev/gym/service/converter/config/ConversionConfiguration.java
@@ -1,9 +1,9 @@
 package dev.gym.service.converter.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
@@ -13,14 +13,14 @@ import java.util.Set;
 
 @Configuration
 @RequiredArgsConstructor
-public class ConversionConfiguration implements ApplicationListener<ApplicationPreparedEvent> {
+public class ConversionConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
     private final Set<Converter<?, ?>> converters;
     private final Set<ConverterFactory<?, ?>> converterFactories;
     private final ConversionService conversionService;
 
     @Override
-    public void onApplicationEvent(ApplicationPreparedEvent event) {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         if (conversionService instanceof GenericConversionService gcs) {
             converters.forEach(gcs::addConverter);
             converterFactories.forEach(gcs::addConverterFactory);
