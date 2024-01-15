ff9c732baad1887f4185b25f37eb6e3e07473f47
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-15 16:52:32 +0500

    Add jpa repositories

--- src/main/java/dev/gym/repository/TrainerRepository.java
+++ src/main/java/dev/gym/repository/TrainerRepository.java
@@ -1,13 +1,16 @@
 package dev.gym.repository;
 
 import dev.gym.repository.model.Trainer;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TrainerRepository extends UserRepository<Trainer, Long>{
+public interface TrainerRepository extends UserRepository<Trainer, Long> {
 
-    List<Trainer> findActiveUnAssignedTrainers();
+    @Query("SELECT t FROM Trainer t WHERE t.isActive = true AND t.trainees IS EMPTY")
+    List<Trainer> findByIsActiveTrueAndAssignedUsersIsEmpty();
 
-    List<Trainer> findTrainersByUsernames(List<String> usernames);
-
+    @Query("SELECT t FROM Trainer t WHERE t.username IN :usernames")
+    List<Trainer> findByUsernameIn(@Param("usernames") List<String> usernames);
 }
