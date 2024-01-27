c8cf5670912edf7509c6ab0b4e3cd08a61368e3b
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-27 19:16:29 +0500

    Add security scheme for swagger endpoints

--- src/main/java/dev/gym/openapi/conf/OpenAPIConfiguration.java
+++ src/main/java/dev/gym/openapi/conf/OpenAPIConfiguration.java
@@ -1,11 +1,22 @@
 package dev.gym.openapi.conf;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static dev.gym.controller.util.RestApiConst.AUTHENTICATION_NAME;
+
 @Configuration
+@SecurityScheme(
+        name = AUTHENTICATION_NAME,
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
 @EnableConfigurationProperties(OpenAPIProperties.class)
 public class OpenAPIConfiguration {
 
