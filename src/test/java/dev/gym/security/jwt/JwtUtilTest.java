b8670e9c07a442f46da059afa46b411f6e92069b
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-01-29 12:10:37 +0500

    Update test according to FIRST principle

--- src/test/java/dev/gym/security/jwt/JwtUtilTest.java
+++ src/test/java/dev/gym/security/jwt/JwtUtilTest.java
@@ -3,9 +3,8 @@ package dev.gym.security.jwt;
 import dev.gym.repository.model.User;
 import dev.gym.security.SecurityUser;
 import io.jsonwebtoken.ExpiredJwtException;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -17,41 +16,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JwtUtilTest {
 
-    private static JwtUtil jwtUtil;
+    private JwtUtil jwtUtil;
+    private String token;
 
-    @BeforeAll
-    static void setUp(){
+    @BeforeEach
+     void setUp(){
         jwtUtil = new JwtUtil();
-        jwtUtil.setSECRET("2dae84f846e4f4b158a8d26681707f4338495bc7ab68151d7f7679cc5e56202dd3da0d356da007a7c28cb0b780418f4f3246769972d6feaa8f610c7d1e7ecf6a");
-        jwtUtil.setEXPIRATION_TIME(86_400_000);
+        jwtUtil.setSecret("2dae84f846e4f4b158a8d26681707f4338495bc7ab68151d7f7679cc5e56202dd3da0d356da007a7c28cb0b780418f4f3246769972d6feaa8f610c7d1e7ecf6a");
+        jwtUtil.setExpirationTime(3600); // 1 hour
+        token = jwtUtil.generateToken("username");
     }
 
     @Test
-    @Order(1)
     void givenUsername_whenGenerateToken_thenOk(){
-        String token = jwtUtil.generateToken("username");
         assertNotNull(token);
     }
 
     @Test
-    @Order(2)
     void givenToken_whenGetUsernameFromToken_thenOk(){
-        String token = jwtUtil.generateToken("username");
         String username = jwtUtil.getUsernameFromToken(token);
         assertEquals("username", username);
     }
 
     @Test
-    @Order(3)
     void givenToken_whenGetExpirationDateFromToken_thenOk() {
-        String token = jwtUtil.generateToken("username");
         assertNotNull(jwtUtil.getExpirationDateFromToken(token));
     }
 
     @Test
-    @Order(4)
     void givenValidToken_whenValidate_thenReturnTrue() {
-        String token = jwtUtil.generateToken("username");
         User user = new User();
         user.setUsername("username");
         boolean isValid = jwtUtil.validateToken(token, new SecurityUser(user));
@@ -59,11 +52,10 @@ class JwtUtilTest {
     }
 
     @Test
-    @Order(5)
     void givenExpiredToken_whenValidate_thenReturnExpiredJwtException() {
         // manually set expiration time to 0 second
-        jwtUtil.setEXPIRATION_TIME(1);
-        String token = jwtUtil.generateToken("username");
+        jwtUtil.setExpirationTime(0);
+        token = jwtUtil.generateToken("username");
         User user = new User();
         user.setUsername("username");
         assertThrows(ExpiredJwtException.class, () -> jwtUtil.validateToken(token, new SecurityUser(user)));
