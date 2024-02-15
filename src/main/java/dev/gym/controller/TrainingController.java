9dccfd8053dd7c98d02ce16a90de5ea52d397ac4
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-02-15 15:22:40 +0500

    Add new method

--- src/main/java/dev/gym/controller/TrainingController.java
+++ src/main/java/dev/gym/controller/TrainingController.java
@@ -1,5 +1,9 @@
 package dev.gym.controller;
 
+import dev.gym.client.workload.ActionType;
+import dev.gym.client.workload.TrainerWorkload;
+import dev.gym.client.workload.TrainerWorkloadClient;
+import dev.gym.service.TrainerService;
 import dev.gym.service.TrainingService;
 import dev.gym.service.TrainingTypeService;
 import dev.gym.service.dto.CreateTrainingDto;
@@ -9,41 +13,53 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import static dev.gym.controller.util.RestApiConst.AUTHENTICATION_NAME;
 import static dev.gym.controller.util.RestApiConst.TRAINING_API_ROOT_PATH;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping(value = TRAINING_API_ROOT_PATH, produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Training API", description = "Operations to manage trainings")
 @SecurityRequirement(name = AUTHENTICATION_NAME) // security requirement for swagger
 @RequiredArgsConstructor
+@Setter
 public class TrainingController {
 
     private final TrainingService trainingService;
+    private final TrainerService trainerService;
     private final TrainingTypeService<TrainingTypeDto> trainingTypeService;
+    private final TrainerWorkloadClient trainerWorkloadClient;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @PostMapping
+    @ResponseStatus(code = CREATED)
     @Operation(summary = "Create a new training")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Training created successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid input"),
             @ApiResponse(responseCode = "409", description = "Training already exists")
     })
-    public ResponseEntity<Void> addTraining(@RequestBody @Valid CreateTrainingDto request) {
+    public void addTraining(@RequestBody @Valid CreateTrainingDto request) {
         trainingService.addTraining(request);
-        return ResponseEntity.ok().build();
+        notifyWorkloadService(request, ActionType.ADD);
     }
 
     @GetMapping("/types")
@@ -54,5 +70,25 @@ public class TrainingController {
     public ResponseEntity<List<TrainingTypeDto>> getTrainingTypes() {
         List<TrainingTypeDto> trainingTypeDtoList = trainingTypeService.getAll();
         return ResponseEntity.ok(trainingTypeDtoList);
+    }
+
+    protected void notifyWorkloadService(CreateTrainingDto trainingDto, ActionType actionType) {
+
+        // get trainer by username
+        trainerService.getByUsername(trainingDto.trainerUsername()).ifPresent(
+                trainerDto -> {
+                    TrainerWorkload trainerWorkload = new TrainerWorkload(
+                            trainerDto.username(),
+                            trainerDto.firstName(),
+                            trainerDto.lastName(),
+                            trainerDto.isActive(),
+                            trainingDto.trainingDate(),
+                            actionType == ActionType.ADD ? trainingDto.trainingDuration() : -trainingDto.trainingDuration(),
+                            actionType);
+
+                    String token = httpServletRequest.getHeader("Authorization");
+                    trainerWorkloadClient.notifyWorkloadService(token, trainerWorkload);
+                }
+        );
     }
 }
