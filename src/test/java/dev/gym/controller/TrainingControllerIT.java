7d473f12e3347528b6e90b8dd5026e7ece9550c0
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-02-15 15:25:01 +0500

    Add tests

---
+++ src/test/java/dev/gym/controller/TrainingControllerIT.java
@@ -0,0 +1,71 @@
+package dev.gym.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.gym.GymApplication;
+import dev.gym.repository.model.enums.TrainingTypeEnum;
+import dev.gym.service.dto.CreateTrainingDto;
+import dev.gym.service.dto.RegisterTraineeDto;
+import dev.gym.service.dto.RegisterTrainerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = GymApplication.class)
+@AutoConfigureMockMvc
+public class TrainingControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static final String traineeFirstName = "traineeFirstName";
+    private static final String traineeLastName = "traineeLastName";
+    private static final String trainerFirstName = "trainerFirstName";
+    private static final String trainerLastName = "trainerLastName";
+
+    @WithMockUser(username = "user")
+    @Test
+    void givenValidRequest_whenCreateTraining_thenCreated() throws Exception {
+        createTrainee();
+        createTrainer();
+
+        CreateTrainingDto createTrainingDto = new CreateTrainingDto(
+            traineeFirstName + "." + traineeLastName,
+            trainerFirstName + "." + trainerLastName,
+            "Test Training",
+            LocalDate.now().plusDays(3),
+            60
+        );
+        mockMvc.perform(post("/api/v1/trainings")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createTrainingDto)))
+                .andExpect(status().isCreated());
+    }
+
+
+    void createTrainer() throws Exception {
+        RegisterTrainerDto trainerDto = new RegisterTrainerDto(trainerFirstName, trainerLastName, TrainingTypeEnum.STRENGTH.toString());
+        mockMvc.perform(post("/api/v1/trainers/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(trainerDto)))
+                .andExpect(status().isCreated());
+    }
+
+    void createTrainee() throws Exception {
+        RegisterTraineeDto traineeDto = new RegisterTraineeDto(traineeFirstName, traineeLastName, LocalDate.now().minusYears(20), "Test Address");
+        mockMvc.perform(post("/api/v1/trainees/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(traineeDto)))
+                .andExpect(status().isCreated());
+    }
+}
--- src/test/java/dev/gym/controller/TrainingControllerTest.java
+++ src/test/java/dev/gym/controller/TrainingControllerTest.java
@@ -1,22 +1,38 @@
 package dev.gym.controller;
 
+import dev.gym.client.workload.ActionType;
+import dev.gym.client.workload.TrainerWorkload;
+import dev.gym.client.workload.TrainerWorkloadClient;
+import dev.gym.repository.model.TrainingType;
+import dev.gym.service.TrainerService;
 import dev.gym.service.TrainingService;
 import dev.gym.service.TrainingTypeService;
 import dev.gym.service.dto.CreateTrainingDto;
+import dev.gym.service.dto.TrainerDto;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.convert.ConversionService;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static dev.gym.controller.util.RestApiConst.TRAINING_API_ROOT_PATH;
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -25,9 +41,13 @@ class TrainingControllerTest {
     @Mock
     private TrainingService trainingService;
     @Mock
+    private TrainerService trainerService;
+    @Mock
     private TrainingTypeService trainingTypeService;
     @Mock
-    private ConversionService conversionService;
+    private TrainerWorkloadClient trainerWorkloadClient;
+    @Mock
+    private HttpServletRequest httpServletRequest;
     @InjectMocks
     private TrainingController trainingController;
 
@@ -74,5 +94,57 @@ class TrainingControllerTest {
         .then()
             .log().ifValidationFails()
             .statusCode(200);
+    }
+
+    @Test
+    void testNotifyWorkloadService_AddAction() {
+        // set httpServletRequest to be used in notifyWorkloadService
+        trainingController.setHttpServletRequest(httpServletRequest);
+
+        CreateTrainingDto trainingDto = new CreateTrainingDto("traineeUsername", "trainerUsername", "Test Training", LocalDate.now().plusDays(2), 60);
+        ActionType actionType = ActionType.ADD;
+
+        TrainingType specialization = mock(TrainingType.class);
+        TrainerDto trainerDto = new TrainerDto("username", "firstName", "lastName", specialization, true, List.of());
+
+        when(trainerService.getByUsername(anyString())).thenReturn(Optional.of(trainerDto));
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("token");
+
+        trainingController.notifyWorkloadService(trainingDto, actionType);
+
+        verify(trainerService, times(1)).getByUsername("trainerUsername");
+        verify(trainerWorkloadClient, times(1)).notifyWorkloadService(eq("token"), any(TrainerWorkload.class));
+    }
+
+    @Test
+    void testNotifyWorkloadService_RemoveAction() {
+        // set httpServletRequest to be used in notifyWorkloadService
+        trainingController.setHttpServletRequest(httpServletRequest);
+
+        CreateTrainingDto trainingDto = new CreateTrainingDto("traineeUsername", "trainerUsername", "Test Training", LocalDate.now().plusDays(2),  60);
+        ActionType actionType = ActionType.DELETE;
+
+        TrainingType specialization = mock(TrainingType.class);
+        TrainerDto trainerDto = new TrainerDto("username", "firstName", "lastName", specialization, true, List.of());
+        when(trainerService.getByUsername(anyString())).thenReturn(Optional.of(trainerDto));
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("token");
+
+        trainingController.notifyWorkloadService(trainingDto, actionType);
+
+        verify(trainerService, times(1)).getByUsername("trainerUsername");
+        verify(trainerWorkloadClient, times(1)).notifyWorkloadService(eq("token"), any(TrainerWorkload.class));
+    }
+
+    @Test
+    void whenBadRequest_thenDontCallTrainerWorkloadClient() {
+        given()
+                .contentType(JSON)
+                .when()
+                .post()
+                .then()
+                .log().ifValidationFails()
+                .statusCode(400);
+
+        verify(trainerWorkloadClient, never()).notifyWorkloadService(anyString(), any(TrainerWorkload.class));
     }
 }
